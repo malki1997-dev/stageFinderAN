ac7c143235831b759055837f1dab964b743aa175
MALKI Mohammed <malki.mohammed.1997@gmail.com>
2025-05-14 11:52:06 +0200

    ajout du header avec notifications static

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { DialogModule } from 'primeng/dialog';
 import { ButtonModule } from 'primeng/button';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./shared/components/header/header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     DialogModule,
-    ButtonModule
-  ],
+    ButtonModule,
+    HeaderComponent
+],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
+  standalone: true,  // 👈 ajoute ça !
+  imports: [
+    CommonModule
+  ],
   selector: 'app-header',
-  imports: [],
-  templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  templateUrl: './header.component.html'
 })
 export class HeaderComponent {
+  notifications = [
+    { message: 'Nouvelle offre de stage disponible', date: 'Aujourd’hui' },
+    { message: 'Ton profil a été visité', date: 'Hier' },
+    { message: 'Ton profil a été visité', date: 'Hier' },
+    { message: 'Rappel : Entretien demain', date: 'Il y a 2 jours' }
+  ];
 
+  showNotifications = false;
+
+  toggleNotifications() {
+    this.showNotifications = !this.showNotifications;
+  }
 }
